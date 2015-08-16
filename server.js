027b2d8f09a171be26774ec5454e1a0e6206c1c3
Alex McMillan <alexmcmillan23@gmail.com>
2015-08-16 15:38:28 +1200

    Crashing heroku.. removed process.env.HOST ??

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.set('port', process.env.PORT || 3000);
-app.set('host', process.env.HOST || '0.0.0.0');
+app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(path.join(__dirname, 'public'), {
 	index: false
@@ -20,6 +19,6 @@ app.set('view engine', 'ejs');
 
 require('./router')(app);
 
-module.exports = app.listen(app.get('port'), app.get('host'), function () {
-	console.log('Listening! ', app.get('host') + ':' + app.get('port'));
+module.exports = app.listen(app.get('port'), function () {
+	console.log('Listening! ', app.get('port'));
 });
